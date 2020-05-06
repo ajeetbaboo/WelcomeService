53e0e19505e7899bf88a6b882eabc741cb65cb0f
Ajeet kumar <ajeetkumarmrj@gmail.com>
2020-05-06 20:37:53 +0530

    First github project.

---
+++ GitTestWeb_1/src/main/java/com/ajeet/git/GitTestWeb1Application.java
@@ -0,0 +1,13 @@
+package com.ajeet.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestWeb1Application.class, args);
+	}
+
+}
